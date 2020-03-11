fde715bf50d833633b4ac3d5780e31b396519165
Ruikang07 <55299854+Ruikang07@users.noreply.github.com>
2020-03-10 23:48:37 -0400

    Update Pathfinding.py

--- Pathfinding.py
+++ Pathfinding.py
@@ -1,1 +1,24 @@
 
+#input include all the grids in it
+input = []
+inputLine = []
+inputGrid = []
+grid = []
+nextGrid = False
+
+with open('pathfinding_a.txt', 'r') as f:
+    for line in f:
+        for x in line:
+            if x != '\n':
+                 inputLine.append(x)
+        inputGrid.append(inputLine)
+        inputLine = []
+    for a in inputGrid:
+        if a == [] and grid != []:
+            input.append(grid)
+            grid = []
+        else:
+            grid.append(a)
+    if grid != []:
+        input.append(grid)
+        grid = []
